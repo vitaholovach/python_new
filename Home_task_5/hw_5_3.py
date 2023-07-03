4551d0e182a89173406ca9f034c42c10e18116fa
vita <132834624+vitaholovach@users.noreply.github.com>
2023-07-03 20:39:52 +0300

    Delete Home_task_5 directory

--- Home_task_5/hw_5_3.py
+++
@@ -1,8 +0,0 @@
-with open('text.txt', 'r') as file:
-    text = file.read()
-result = set()
-for letters in text.lower():
-    if letters.isalpha():
-        result.update({f'{letters} - {text.count(letters)}'})
-for letters in result:
-    print(letters)
